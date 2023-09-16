c273ee62210c614922e0ac1524f733159482d622
Tomoya-P <tomoyaphong@gmail.com>
2023-09-16 02:30:43 -0600

    first commit

---
+++ problem-1.java
@@ -0,0 +1,11 @@
+class Problem1 {
+  public static void main(String args[]) {
+    int sum = 0;
+    for(int a = 0; a < 1000; a++){
+      if(a % 3 == 0 || a % 5 == 0){
+        sum += a;
+      }
+    }
+    System.out.println("Final sum: " + sum);
+  }
+}
